fef0fd3595ade20698d2468b4fa6adae3696155f
ankitabmungalpara <mungalpara.ankita@gmail.com>
2025-01-12 00:49:51 +0530

    Create market-analysis-ii.sql

---
+++ market-analysis-ii.sql
@@ -0,0 +1,113 @@
+"""
+  
+Table: Users
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| join_date      | date    |
+| favorite_brand | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) of this table.
+This table has the info of the users of an online shopping website where users can sell and buy items.
+ 
+Table: Orders
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| order_id      | int     |
+| order_date    | date    |
+| item_id       | int     |
+| buyer_id      | int     |
+| seller_id     | int     |
++---------------+---------+
+order_id is the primary key (column with unique values) of this table.
+item_id is a foreign key (reference column) to the Items table.
+buyer_id and seller_id are foreign keys to the Users table.
+ 
+Table: Items
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| item_id       | int     |
+| item_brand    | varchar |
++---------------+---------+
+item_id is the primary key (column with unique values) of this table.
+ 
+
+Write a solution to find for each user whether the brand of the second item (by date) they sold is their favorite brand. 
+If a user sold less than two items, report the answer for that user as no. It is guaranteed that no seller sells more than one item in a day.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+Input: 
+Users table:
++---------+------------+----------------+
+| user_id | join_date  | favorite_brand |
++---------+------------+----------------+
+| 1       | 2019-01-01 | Lenovo         |
+| 2       | 2019-02-09 | Samsung        |
+| 3       | 2019-01-19 | LG             |
+| 4       | 2019-05-21 | HP             |
++---------+------------+----------------+
+Orders table:
++----------+------------+---------+----------+-----------+
+| order_id | order_date | item_id | buyer_id | seller_id |
++----------+------------+---------+----------+-----------+
+| 1        | 2019-08-01 | 4       | 1        | 2         |
+| 2        | 2019-08-02 | 2       | 1        | 3         |
+| 3        | 2019-08-03 | 3       | 2        | 3         |
+| 4        | 2019-08-04 | 1       | 4        | 2         |
+| 5        | 2019-08-04 | 1       | 3        | 4         |
+| 6        | 2019-08-05 | 2       | 2        | 4         |
++----------+------------+---------+----------+-----------+
+Items table:
++---------+------------+
+| item_id | item_brand |
++---------+------------+
+| 1       | Samsung    |
+| 2       | Lenovo     |
+| 3       | LG         |
+| 4       | HP         |
++---------+------------+
+Output: 
++-----------+--------------------+
+| seller_id | 2nd_item_fav_brand |
++-----------+--------------------+
+| 1         | no                 |
+| 2         | yes                |
+| 3         | yes                |
+| 4         | no                 |
++-----------+--------------------+
+  
+"""
+
+with cte as(
+  select 
+    *, 
+    rank() over(partition by seller_id order by order_date) as rnk
+  from 
+    orders 
+),
+cte2 as (
+  select 
+    seller_id, i.item_id, rnk, i.item_brand
+  from 
+    cte c join items i
+  on 
+    c.item_id = i.item_id
+  where 
+    rnk = 2
+)
+select 
+  u.user_id as seller_id,
+case
+    when favorite_brand = item_brand then "yes"
+    else "no"
+    end as 2nd_item_fav_brand
+from 
+  users u left join cte2 c
+on 
+  u.user_id = c.seller_id
